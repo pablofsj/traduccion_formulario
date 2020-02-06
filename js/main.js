0c77fe7498c80111fcef672d0f696f414446db67
pablofsj <saintjourpablo@gmail.com>
2020-02-06 12:34:23 -0300

    archivos modificados

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var h2=document.getElementById("form-signin-heading");
+h2.innerHTML="Por favor inicia sesión";
+
+var ingresarmail=document.getElementById("inputEmail");
+ingresarmail.placeholder="Correo Electrónico";
+
+var ingresarpass=document.getElementById("inputPassword");
+ingresarpass.placeholder="Contraseña";
+
+var recuerdame=document.getElementsByTagName("span")[0];
+recuerdame.innerHTML="Recordar datos";
+
+var boton=document.getElementsByTagName("button")[0];
+boton.innerHTML="Iniciar Sesión";
